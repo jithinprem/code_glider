39ee38ddba8505d2754525da59ae81ba263bc515
Jithin <jithinprem43@gmail.com>
2022-08-26 09:59:48 +0530

    firstcommit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class NaviPage extends StatefulWidget {
+  const NaviPage({Key? key}) : super(key: key);
+
+  @override
+  State<NaviPage> createState() => _NaviPageState();
+}
+
+class _NaviPageState extends State<NaviPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // navigator
+      initialRoute: ,
+    );
+  }
+}
